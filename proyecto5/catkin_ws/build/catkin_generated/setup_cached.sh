1d3e7e6d8a217b32bcb38e90dee08cb42821c06d
yago <yago.casasnovas@gmail.com>
2017-11-07 19:43:29 +0100

    s

---
+++ proyecto5/catkin_ws/build/catkin_generated/setup_cached.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/yago/cursoRobotics/proyecto5/catkin_ws/devel:$CMAKE_PREFIX_PATH"
+export LD_LIBRARY_PATH="/home/yago/cursoRobotics/proyecto5/catkin_ws/devel/lib:/home/yago/cursoRobotics/proyecto4/catkin_ws/devel/lib:/home/yago/cursoRobotics/proyecto3/catkin_ws/devel/lib:/opt/ros/lunar/lib:/opt/ros/lunar/lib/x86_64-linux-gnu"
+export PKG_CONFIG_PATH="/home/yago/cursoRobotics/proyecto5/catkin_ws/devel/lib/pkgconfig:/home/yago/cursoRobotics/proyecto4/catkin_ws/devel/lib/pkgconfig:/home/yago/cursoRobotics/proyecto3/catkin_ws/devel/lib/pkgconfig:/opt/ros/lunar/lib/pkgconfig:/opt/ros/lunar/lib/x86_64-linux-gnu/pkgconfig"
+export PWD="/home/yago/cursoRobotics/proyecto5/catkin_ws/build"
+export ROSLISP_PACKAGE_DIRECTORIES="/home/yago/cursoRobotics/proyecto5/catkin_ws/devel/share/common-lisp:$ROSLISP_PACKAGE_DIRECTORIES"
+export ROS_PACKAGE_PATH="/home/yago/cursoRobotics/proyecto5/catkin_ws/src:$ROS_PACKAGE_PATH"
